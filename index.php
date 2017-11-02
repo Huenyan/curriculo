4dcebf2478564adc05ad697da61a88141b0b8cba
Ronyan Lucietti <ronyan_lucietti@hotmail.com>
2017-11-02 21:18:23 -0200

    teste_email

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php header( 'Location: /index.html' ) ;  
 
-if(isset($_POST['btn'])){
-    $name=$_POST['nome'];
-    $email=$_POST['email'];
-    $phone=$_POST['telefone'];
-    $msg=$_POST['texto'];
 
-    $to='ronyanlu@gmail.com';
-    $subject='Form Submission';
-    $message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-    $headers="From: ".$email;
-
-    if(mail($to, $subject, $message, $headers)){
-        echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-    }
-    else{
-        echo "Something went wrong!";
-    }
-}
 
 ?>
