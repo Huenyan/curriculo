78d25b340a799e1ab75e7dd138c23837e5c83c35
Ronyan Lucietti <ronyan_lucietti@hotmail.com>
2017-11-02 21:13:09 -0200

    teste_email

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
-<?php
-	if(isset($_POST['btn'])){
-		$name=$_POST['nome'];
-		$email=$_POST['email'];
-		$phone=$_POST['telefone'];
-		$msg=$_POST['texto'];
+<?php header( 'Location: /index.html' ) ;  
 
-		$to='ronyanlu@gmail.com';
-		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
-		$headers="From: ".$email;
+if(isset($_POST['btn'])){
+    $name=$_POST['nome'];
+    $email=$_POST['email'];
+    $phone=$_POST['telefone'];
+    $msg=$_POST['texto'];
 
-		if(mail($to, $subject, $message, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
-	}
+    $to='ronyanlu@gmail.com';
+    $subject='Form Submission';
+    $message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+    $headers="From: ".$email;
+
+    if(mail($to, $subject, $message, $headers)){
+        echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+    }
+    else{
+        echo "Something went wrong!";
+    }
+}
+
 ?>
