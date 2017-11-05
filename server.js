915677f5636363ac2eb72c2f929a68bb030781da
Ronyan Lucietti <ronyan_lucietti@hotmail.com>
2017-11-05 17:35:11 -0200

    try fix css 3

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var express=require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
 var nodemailer = require("nodemailer");
-var app=express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+//Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//Body Parser Muddleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.render('curriculo');
+});
 
 var smtpTransport = nodemailer.createTransport({
     service: "gmail",
